2a26674fce3f17faf8d2dce440fde70efb2de61c
jacobyoldas <yasinyoldas6666@gmail.com>
2023-03-03 19:24:09 -0500

    new class added and continued test case

---
+++ src/test/java/technostudyB7/BasePOM.java
@@ -0,0 +1,108 @@
+package technostudyB7;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import java.util.ArrayList;
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BasePOM {
+
+  static WebDriver driver;
+  static WebDriverWait wait;
+  static WebElement element;
+
+  @BeforeMethod
+  public static void setDriver(){
+
+    WebDriverManager.chromedriver().setup();
+
+    driver = new ChromeDriver();
+
+    driver.manage().window().maximize();
+
+    wait= new WebDriverWait(driver,3);
+
+    driver.get("http://demo.seleniumeasy.com/");
+  }
+
+  @AfterMethod
+  public static void quitDriver() {
+
+    if (driver!= null) // That means  if there is still driver then quit
+      driver.quit();
+    System.out.println("Test is Done");
+  }
+
+  public static void setWait(int second) {
+    try {
+      Thread.sleep(second* 1000L);
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public static void clickLinksSubCategory(String category, String subCategory)
+  {
+    element = driver.findElement(
+        By.xpath("//div[@id='navbar-brand-centered']//a[contains(text(),'" + category + "')]"));
+
+    element.click();
+
+
+    element = driver.findElement(
+        By.xpath("//div[@id='navbar-brand-centered']//a[contains(text(),'" + subCategory + "')]"));
+
+    element.click();
+
+  }
+
+  public static void clickLinks(String category) {
+
+    element = driver.findElement(
+        By.xpath("//div[@id='navbar-brand-centered']//a[contains(text(),'" + category + "')]"));
+
+    element.click();
+
+  }
+
+  public static List<String> getActualDropDownLists() {
+
+    element = driver.findElement(By.id("select-demo"));
+
+    Select select= new Select(element);
+
+    List<String> textOptions = new ArrayList<>();
+
+    for ( WebElement option: select.getOptions()) {
+      textOptions.add(option.getText());
+    }
+    return textOptions;
+
+  }
+
+  public static void waitUntilElementVisibleAndClickable(){
+    wait.until(ExpectedConditions.visibilityOf(element));
+    wait.until(ExpectedConditions.visibilityOfAllElements(element));
+    wait.until(ExpectedConditions.elementToBeClickable(element));
+  }
+
+  public static void scrollToElement(){
+    JavascriptExecutor js= (JavascriptExecutor) driver;
+    js.executeScript("arguments[0].scrollIntoView();", element);
+    setWait(3);
+    //js.executeScript("window.scrollBy(0,1000)");
+//    js.executeScript("arguments[0].scroll(0,3000);", element);
+//    js.executeScript("arguments[0].click();", element);
+
+  }
+
+}
--- src/test/java/technostudyB7/SeleniumMentoring.java
+++ src/test/java/technostudyB7/SeleniumMentoring.java
@@ -1,119 +1,50 @@
 package technostudyB7;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import static technostudyB7.BasePOM.clickLinksSubCategory;
+import static technostudyB7.BasePOM.getActualDropDownLists;
+
 import java.util.Arrays;
 import java.util.List;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SeleniumMentoring {
 
-
   /**
-   * Q/A: Who/Whom responsible to create test cases/scenario/plan? Test Case "COVERAGES" and captain of the ship(ownerShip)
-
-   * Test Case Preparation
-   * 1- Open the website
-   * 2- Test Header links(Category) is working
-   * 3- Test Sub category links are working
-   *New Test Case
-   *  * MAGIC TIME (Shift+Alt)
-   *      *handle Thread sleep with method (try-catch)
-   *      * add web-driver wait
-   *      * add waitUntilVisibleAndClickable method
-   *      * add quit driver
-   *      * handle sub category (work for single and double click)
-   *      * HOW TO BECOME better QA 3+ or 5+ exp handling the code look (My interview EXP)
-   *     Test Case: Dropdown list
-   *      * * 1- Set expected result/requirements
-   *      * * 2- Get actual result/requirements
-   *      * * 3- Validate/Verify  expected vs. actual result/requirements
-   *      * * 3- Set actual result/requirements
-   *      * * 2- Validate/Verify  expected vs. actual result/requirements
+   * Q/A: Who/Whom responsible to create test cases/scenario/plan? Test Case "COVERAGES" and captain
+   * of the ship(ownerShip)
+   * <p>
+   * Test Case 1 Preparation 1- Open the website 2- Test Header links(Category) is working 3- Test
+   * Sub category links are working New Test Case * MAGIC TIME (Shift+Alt) *handle Thread sleep with
+   * method (try-catch) * add web-driver wait * add waitUntilVisibleAndClickable method * add quit
+   * driver * handle sub category (work for single and double click) * HOW TO BECOME better QA 3+ or
+   * 5+ exp handling the code look (My interview EXP) Test Case 2: Dropdown list 1- Set expected
+   * result/requirements 2- Get actual result/requirements 3- Validate/Verify Dropdown list is there
+   * 4- Set actual result/requirements( as end user perspective) 5- Validate/Verify  expected vs.
+   * actual result/requirements Test Case 3: Header And Footer 1- Set expected results 2- Get actual
+   * results 3- Validate links are there 4- Validate clink functions are working
    */
 
-    static WebDriver driver;
-    static WebDriverWait wait;
 
+  public static void main(String[] args) {
 
-    public static void main(String[] args) {
+    clickLinksSubCategory("Input Forms", "Select Dropdown List");
 
-      WebDriverManager.chromedriver().setup();
+    List<String> expectedList = Arrays.asList("Please select", "Sunday", "Monday", "Tuesday",
+        "Wednesday", "Thursday", "Friday", "Saturday");
 
-      driver = new ChromeDriver();
+    List<String> actualList = getActualDropDownLists();
 
-      driver.manage().window().maximize();
-
-      wait= new WebDriverWait(driver,3);
-
-      driver.get("https://demo.seleniumeasy.com/");
-
-      setWait(3);
-
-      clickLinksSubCategory("Input Forms","Checkbox Demo");
-
-      clickLinksSubCategory("Input Forms","Select Dropdown List");
-
-      List<String> expectedList= Arrays.asList("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
-
-
-
-
-
-      quitDriver();
-
-
-
-
+    if (expectedList.equals(actualList)) {
+      System.out.println("Passed");
+    } else {
+      System.out.println("Fail");
     }
 
+    //@AfterMethod and @BeforeMethod why did not work?
+    // 1- I am missing a keyword
+    // 2- Where/What method should I run my test?
+    // 3- TestNG What's for?
 
-
-    private static void quitDriver() {
-
-      if (driver!= null) // That means  if there is still driver then quit
-        driver.quit();
-      System.out.println("Test is Done");
-    }
-
-    private static void setWait(int second) {
-      try {
-        Thread.sleep(second* 1000L);
-      } catch (InterruptedException e) {
-        throw new RuntimeException(e);
-      }
-    }
-
-    private static void clickLinksSubCategory(String category, String subCategory)
-    {
-      WebElement element = driver.findElement(
-          By.xpath("//div[@id='navbar-brand-centered']//a[contains(text(),'" + category + "')]"));
-
-      wait.until(ExpectedConditions.visibilityOf(element));
-      element.click();
-
-      setWait(3);
-
-      WebElement element2 = driver.findElement(
-          By.xpath("//div[@id='navbar-brand-centered']//a[contains(text(),'" + subCategory + "')]"));
-
-      element2.click();
-
-    }
-
-    public static void clickLinks(String category) {
-
-      WebElement element = driver.findElement(
-          By.xpath("//div[@id='navbar-brand-centered']//a[contains(text(),'" + category + "')]"));
-
-      element.click();
-
-    }
-
+  }
 
 
 }
