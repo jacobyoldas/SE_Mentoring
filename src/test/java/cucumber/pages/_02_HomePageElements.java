2fc0d282a6c0005a279ce659654b85ea48a2945b
jacobyoldas <yasinyoldas6666@gmail.com>
2023-03-30 21:31:16 -0400

    classes update

--- src/test/java/cucumber/pages/_02_HomePageElements.java
+++ src/test/java/cucumber/pages/_02_HomePageElements.java
@@ -4,7 +4,6 @@ import cucumber.utilities.BaseMethods;
 import cucumber.utilities.Driver_Base;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.PageFactory;
-import org.testng.Assert;
 
 public class _02_HomePageElements extends BaseMethods {
   public _02_HomePageElements() {
@@ -12,12 +11,13 @@ public class _02_HomePageElements extends BaseMethods {
   }
 
 
-  public static By successMessage = By.cssSelector ("div.custom.carousel-caption p");
+  public static By loginSuccessMessage = By.cssSelector ("div.custom.carousel-caption p");
+
 
 
   public void successMessageDisplayed() {
     String expectedResult="Welcome";
 
-    assertTrueValidationText(successMessage,expectedResult);
+    assertTrueValidationText(loginSuccessMessage,expectedResult);
   }
 }
